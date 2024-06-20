83f0ea2159d8dc8c029f62c299cfb6e0922124ff
miguel <weigertmiguel@hotmail.com>
2024-06-20 18:41:48 -0300

    Primeiro teste unitário

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
 import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
 import { hash } from "bcryptjs";
 
 //  services
@@ -10,16 +11,20 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
-// SOLID - Uncle Bob (Clean Code)
-
-// D - Dependency Inversion Principle
+interface RegisterUseCaseResponse {
+  user: User;
+}
 
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {
     // inversão de dependências
   }
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -28,10 +33,13 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+    return {
+      user,
+    };
   }
 }
