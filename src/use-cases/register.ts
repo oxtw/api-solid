cc95308e8d4a380deaf850e21fc31d71b8a2d8a0
miguel <weigertmiguel@hotmail.com>
2024-06-20 00:49:00 -0300

    Caso de uso de registro

---
+++ src/use-cases/register.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6);
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    throw new Error("E-mail already exists");
+
+    // return reply.status(409).send(); //HTTP
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  });
+}
