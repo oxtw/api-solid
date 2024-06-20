b06bbfb986d59e29149ddb7dfd9f0010bf48e469
miguel <weigertmiguel@hotmail.com>
2024-06-20 16:04:15 -0300

    Configurando Vitest

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
-import { prisma } from "@/lib/prisma";
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 
