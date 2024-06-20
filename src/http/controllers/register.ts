31bd1251268741eef8b4d8605dbcfb43c9744b53
miguel <weigertmiguel@hotmail.com>
2024-06-20 00:31:26 -0300

    Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -13,6 +13,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   const password_hash = await hash(password, 6);
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
 
   await prisma.user.create({
     data: {
