6baaa1ffb5ac9dee498d6d1ec40be2a0f995a67b
miguel <weigertmiguel@hotmail.com>
2024-06-24 16:12:57 -0300

    In-Memory Databases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,7 +2,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { RegisterUseCase } from "@/use-cases/register";
-import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
